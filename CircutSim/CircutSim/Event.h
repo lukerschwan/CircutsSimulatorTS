c3c9ba147dbef2aa9e4a48dcc4ae0dadec825cdf
John Towner <johnnytowner@gmail.com>
2018-04-17 09:44:39 -0400

    Work on event

--- CircutSim/CircutSim/Event.h
+++ CircutSim/CircutSim/Event.h
@@ -1,5 +1,17 @@
 #pragma once
-
-Class Event{
+#include "Wire.h"
+using namespace std;
+class Event{
 	//added event class  x
-}
+
+public:
+	void getEventWire();
+
+
+private:
+
+	Wire * eventWire;
+	int time;
+	int val;
+	int creationCnt;
+};
