747d3ef3a9bc202358d8e973b532e449bdbc68aa
John Towner <johnnytowner@gmail.com>
2018-04-17 09:50:37 -0400

    more with event.h and created event.cpp

--- CircutSim/CircutSim/Event.h
+++ CircutSim/CircutSim/Event.h
@@ -2,11 +2,16 @@
 #include "Wire.h"
 using namespace std;
 class Event{
-	//added event class  x
-
+	//added event class
 public:
-	void getEventWire();
-
+	void setEventWire(Wire &w);
+	void setTime(int t);
+	void setVal(int v);
+	void setCreationCnt(int c);
+	Wire *getEventWire() const;
+	int getTime() const;
+	int getVal() const;
+	int getCreationCnt() const;
 
 private:
 
