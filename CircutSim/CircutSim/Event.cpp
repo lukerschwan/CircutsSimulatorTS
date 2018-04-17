ea7a371bd1a140dc9b0400b62cd594dc40aa7b20
John Towner <johnnytowner@gmail.com>
2018-04-17 11:04:01 -0400

    implemented event.cpp

--- CircutSim/CircutSim/Event.cpp
+++ CircutSim/CircutSim/Event.cpp
@@ -2,40 +2,48 @@
 
 Event::Event(Wire & w, int t, int v, int c)
 {
+	eventWire = &w;
+	time = t;
+	val = v;
+	creationCnt = c;
 }
 
 void Event::setEventWire(Wire & w)
 {
+	eventWire = &w;
 }
 
 void Event::setTime(int t)
 {
+	time = t;
 }
 
 void Event::setVal(int v)
 {
+	val = v;
 }
 
 void Event::setCreationCnt(int c)
 {
+	creationCnt = c;
 }
 
 Wire * Event::getEventWire() const
 {
-	return nullptr;
+	return eventWire;
 }
 
 int Event::getTime() const
 {
-	return 0;
+	return time;
 }
 
 int Event::getVal() const
 {
-	return 0;
+	return val;
 }
 
 int Event::getCreationCnt() const
 {
-	return 0;
+	return creationCnt;
 }
